cfbe91f414437c9a8d39c95012c2f2eae2a02daa
skuzmin <sergey.kuzmin@motorsport.com>
2024-11-13 13:55:23 +0500

    Fix eslint.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import Form from "./modules/Form";
 
 const template = { name: 'rob', job: 'hexlet', gender: 'm' };
 
-console.log(new Form().formFor(template, {}, (f) => {}));
+console.log(new Form().formFor(template, {}, () => {}));
 
 export default new Form();
