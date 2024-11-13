417521c5323a25e227ae92ba9be0966dd500e637
skuzmin <sergey.kuzmin@motorsport.com>
2024-11-13 12:53:47 +0500

    Fix tests for Tag

--- src/tests/Tag.test.ts
+++ src/tests/Tag.test.ts
@@ -1,8 +1,26 @@
-import Tag from "src/modules/Tag";
+import Tag from '../modules/Tag';
 import { expect, test } from 'vitest';
 
 test('create tag div with value', () => {
     const testTag = new Tag('div', {}, 'test case 1');
 
-    expect (testTag.toString()).toBe('<div>test case 1</div>');
+    expect(testTag.toString()).toBe('<div>test case 1</div>');
+});
+
+test('create tag div with value and attribs', () => {
+    const testTag = new Tag('div', { for: 'me' }, 'test case 2');
+
+    expect(testTag.toString()).toBe('<div for="me">test case 2</div>');
+});
+
+test('create unsupported tag', () => {
+    const testTag = new Tag('dive', { for: 'me' }, 'test case 3');
+
+    expect(testTag.toString()).toBe('Unsupported tag - dive');
+});
+
+test('create single tag', () => {
+    const testTag = new Tag('img');
+
+    expect(testTag.toString()).toBe('<img/>');
 });
